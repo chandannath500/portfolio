6052fe77d2ccce1a96504a3957e0043540dae631
chandan <chandannath500@gmail.com>
2021-05-07 16:53:19 +0530

    burgerapp added

---
+++ build/precache-manifest.ac9af9af733098865d1b240c7d0b6fc3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f31d3b89e76d0dcf50eb0a6bc32a087",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c938639b58cc07a87a2",
+    "url": "/static/css/main.c299df2a.chunk.css"
+  },
+  {
+    "revision": "164342fae57cba3569d0",
+    "url": "/static/js/2.448f8912.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.448f8912.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c938639b58cc07a87a2",
+    "url": "/static/js/main.d470e3c2.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "b2cd90df6e19261627b434eef9fd94ec",
+    "url": "/static/media/ColorChange.b2cd90df.png"
+  },
+  {
+    "revision": "9e29acfb5ae7feec3e14afee150cf0ea",
+    "url": "/static/media/Keylogger.9e29acfb.png"
+  },
+  {
+    "revision": "f53267e7fc3bb25206d3c09d0622738f",
+    "url": "/static/media/Upcoming.f53267e7.png"
+  },
+  {
+    "revision": "845ef3a6cff5203a29d0b38c61cb7e2f",
+    "url": "/static/media/burger.845ef3a6.png"
+  },
+  {
+    "revision": "4ee9fc33a5e548acea1f1dd3893f3611",
+    "url": "/static/media/demo.4ee9fc33.png"
+  },
+  {
+    "revision": "7d7ba0298bbbb2c74911b7b7a8621a6e",
+    "url": "/static/media/passlist.7d7ba029.png"
+  },
+  {
+    "revision": "836741df44c689fee3801d1545cb7b8d",
+    "url": "/static/media/video.836741df.mov"
+  }
+]);
---
+++ src/Main/data.jsx
@@ -0,0 +1,90 @@
+export const dataExperience = {
+    list: {
+        0: {
+            main: 'IT Executive',
+        },
+        1: {
+            main: 'IT Sales & Development',
+        },
+        2: {
+            main: 'Guest Lecturer',
+        },
+        3: {
+            main: 'Freelancer',
+        },
+        // 4: {
+        //     main: 'Cyber-Security',
+        // },
+        // 5: {
+        //     main: 'Dog Trainer',
+        // },
+    },
+    info: {
+        0: {
+            link: '',
+            title: 'IT Executive @ Shri Mahavir Ferro Alloys Pvt. Ltd',
+            date: 'Feb 2017 - Present',
+            first: 'Developed in-house apps ',
+            second: 'I designed and created software solutions to solve pain points',
+            third: 'Checked for feasibility of web/software prototypes and modified codes to fix errors',
+        },
+        1: {
+            link: '',
+            title: 'IT Sales & Development @ BIOS Technology',
+            date: 'Jun 2013 - 2016',
+            first: "Negotiating sales, prices, contracts and payments",
+            second: 'Assess client IT needs and makes recommendations, including IT equipment and service packages',
+            third: 'Advising clients on technical queries relating to products, installations and relevant upgrades',
+        },
+        2: {
+            link: '',
+            title: 'Lecturer @ Amravati College Of Engineering & Technology',
+            date: 'Apr 2011 - 2013',
+            first: 'Teaching various subjects of Computer Science & Engineering.',
+            second: '',
+            third: '',
+        },
+        3: {
+            link: '',
+            title: 'Freelancer',
+            date: 'Jul 2010 - present',
+            first: 'My work corresponds to use creative and software engineering/programming skills to design, build and improve websites',
+            second: '',
+            third: '',
+        },
+        // 4: {
+        //     link: 'http://www.unitesict.com/',
+        //     title: '@UNITeS Cisco 🔐',
+        //     date: 'Intern | Sept 2020 - Nov 2020',
+        //     first: 'My work corresponds to a fall internship while studying in college.',
+        //     second: 'A Cisco Virtual Graduate Internship on Cyber-Security',
+        // },
+        // 5: {
+        //     link: '',
+        //     title: 'Dog Trainer 🐕',
+        //     date: 'Sep 2013 - May 2015',
+        //     first: 'I worked and helped train our awesome canine friends when I was around 16 years while I was just rounding up High-School.',
+        // },
+    },
+};
+
+export const dataEducation = {
+    0: {
+        main: 'SAIERC 🎓',
+        expand: 'Matriculation',
+        date: '( 1990 / 2003 )',
+        place: 'Rajgangpur',
+    },
+    1: {
+        main: 'Govt. College 🎓',
+        expand: 'Science (Intermediate)',
+        date: '( 2003 / 2005 )',
+        place: 'Rourkela',
+    },
+    2: {
+        main: 'PIET 🎓',
+        expand: 'B.Tech in CSE',
+        date: '( 2006 / 2010 )',
+        place: 'Mandiakudar',
+    },
+};
